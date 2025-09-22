beaf32b4c6a440b515558573a26d8a8887498ca9
Miguel Ángel Latre Abadía <latre@unizar.es>
2025-09-22 13:54:06 +0200

    Update file headers and comments to 79 chars

--- tablas.cpp
+++ tablas.cpp
@@ -1,10 +1,10 @@
-/*******************************************************************************
+/******************************************************************************
  * Curso de Programación 1. Práctica 2
  * Autores: Miguel Ángel Latre y Javier Martínez
  * Última revisión: 22 de septiembre de 2022
  * Resumen: Programa interactivo que presenta por pantalla las tablas de 
  *          multiplicar seleccionadas por el usuario.   
- ******************************************************************************/
+ *****************************************************************************/
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -37,27 +37,27 @@ void presentarTabla(unsigned n) {
 
 /*
  * Programa que pregunta reiteradamente al usuario qué tabla de multiplicar 
- * desea escribir y la escribe a continuación, salvo cuando el usuario responde 
+ * desea escribir y la escribe a continuación, salvo cuando el usuario responde
  * con un 0, en cuyo caso el programa termina.
  */
 int main() {
-    // Plantea la primera pregunta al usuario
+    // Plantea la primera pregunta al usuario.
     cout << "Tabla que desea escribir (" << FIN << " para acabar): ";
 
-    // Asigna a «multiplicando» el primer valor entero escrito por el usuario
+    // Asigna a «multiplicando» el primer valor entero escrito por el usuario.
     unsigned multiplicando;
     cin  >>  multiplicando;
 
     // Itera hasta que el usuario responda con un valor nulo
     while (multiplicando != FIN) {
-        // Presenta la tabla de multiplicar del último dato escrito por el usuario.
+        // Presenta la tabla de multiplicar del último dato dado por el usuario
         presentarTabla(multiplicando);
 
-        // Plantea una nueva pregunta al usuario
+        // Plantea una nueva pregunta al usuario.
         cout << endl;
         cout << "Tabla que desea escribir (" << FIN << " para acabar): ";
 
-        // Asigna a «multiplicando» el nuevo valor entero escrito por el usuario
+        // Asigna a «multiplicando» el nuevo valor escrito por el usuario.
         cin >> multiplicando;
     }
 }
